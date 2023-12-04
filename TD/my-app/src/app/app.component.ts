96aae53bbd1dc21e87f02af7dc489a1c447c899e
pelleservan <pelle.servan@hotmail.fr>
2023-12-04 16:04:11 +0100

    add TD2 counter

--- TD/my-app/src/app/app.component.ts
+++ TD/my-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { CountComponent } from './count/count.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HelloWorldComponent],
+  imports: [CommonModule, RouterOutlet, HelloWorldComponent, CountComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ TD/my-app/src/app/count/count.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-count',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './count.component.html',
+  styleUrls: ['./count.component.scss']
+})
+export class CountComponent {
+  counter: number = 0;
+  disableIncrementButton: boolean = false;
+
+  incrementCounter(): void {
+    this.counter++;
+  }
+
+  toggleIncrementButton(): void {
+    this.disableIncrementButton = !this.disableIncrementButton;
+  }
+}
